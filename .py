b14241c1e794170466a07a82974b52f6bac5603b
Ravitej29 <68075093+Ravitej29@users.noreply.github.com>
2020-09-16 11:19:38 +0530

    Update .py

--- .py
+++ .py
@@ -1,1 +1,12 @@
+list=[1,13,2,15,12]
+def selection_sort(a):
+	for j in range(len(a)-1):
+		minimum = j
+		for i in range(j+1, len(a)):  
+			if(a[i]<a[minimum]):
+				minimum = i 
+		a[j],a[minimum]=a[minimum],a[j] 
+	print("after selection sort:")
+	print(a)
 
+selection_sort(list)
